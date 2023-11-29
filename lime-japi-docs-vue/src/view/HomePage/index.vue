7a7aa6685072ed7c84b78f10df6040d675a8674e
xctx <Xm19981029>
2023-11-29 17:58:18 +0800

    前端：home页面调整了几个字

--- lime-japi-docs-vue/src/view/HomePage/index.vue
+++ lime-japi-docs-vue/src/view/HomePage/index.vue
@@ -3,11 +3,18 @@
     <div class="page-header-bg">
         <a-row class="page-header">
             <a-col :span="12">
-                <lime-logo :height="65" :open-click="true"/>
+                <lime-logo :height="70" :open-click="true" style="margin-top: 5px"/>
             </a-col>
             <a-col :span="12" style="text-align: right">
-                <a-space align="center" size="middle" class="header-func-box">
-
+                <a-space align="center" size="large" class="header-func-box">
+                    <div>
+                        <a-cascader :options="[{value: 'V1.0.1',label: 'V1.0.1'}, {value: 'V1.1.1',label: 'V1.1.1'}]">
+                            <a href="#">版本</a>
+                        </a-cascader>
+                    </div>
+                    <div style="height: 80px">
+                        <GiteeBox />
+                    </div>
                 </a-space>
             </a-col>
         </a-row>
@@ -30,7 +37,7 @@
     background: #fff;
     height: 80px;
     padding: 0 25px;
-    font-size: 20px;
+    font-size: 17px;
     position: fixed;
     border-bottom: 1px solid #eee;
     box-shadow: 2px 2px 25px rgba(0,0,0,.05);
